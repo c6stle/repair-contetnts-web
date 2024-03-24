447dd48b106f6ea209f38db82695fecff43016eb
rainn66 <jaeseong7975@gmail.com>
2024-03-24 22:01:45 +0900

    2024.03.24

--- src/main/java/webml/core/common/JsonController.java
+++ src/main/java/webml/core/common/JsonController.java
@@ -3,6 +3,7 @@ package webml.core.common;
 import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import webml.core.exception.MessageException;
 import webml.core.util.CustomMap;
 
 @Controller
@@ -10,47 +11,46 @@ import webml.core.util.CustomMap;
 public class JsonController {
 
     @ResponseBody
+    @GetMapping("/{s}/{m}")
+    public Object get(@RequestParam CustomMap param,
+                      @PathVariable("s") String serviceName,
+                      @PathVariable("m") String methodName) throws Exception {
+        //서비스명 파싱
+        String serviceClassName = serviceName.substring(0, 1).toUpperCase() + serviceName.substring(1) + "Service";
+
+        //서비스 인스턴스 생성/반환
+        return this.serviceMethod(param, serviceClassName, methodName);
+    }
+
+
+    @ResponseBody
     @PostMapping("/{s}/{m}")
     public Object post(@RequestBody CustomMap param,
                        @PathVariable("s") String serviceName,
                        @PathVariable("m") String methodName) throws Exception {
-        //return
-
-        //서비스명 파싱
-        String serviceClassName = serviceName.substring(0, 1).toUpperCase() + serviceName.substring(1, serviceName.length()) + "Service";
-
-        //서비스 인스턴스 생성/반환
+        String serviceClassName = serviceName.substring(0, 1).toUpperCase() + serviceName.substring(1) + "Service";
         return this.serviceMethod(param, serviceClassName, methodName);
     }
 
     private Object serviceMethod(CustomMap param, String serviceClassName, String methodName) throws Exception {
         Object rtn;
 
-        Class<?> classObj = Class.forName("webml.service." + serviceClassName);
-        Object service = classObj.getDeclaredConstructor().newInstance();
+        try {
+            Class<?> classObj = Class.forName("webml.service." + serviceClassName);
+            Object service = classObj.getDeclaredConstructor().newInstance();
 
-        //Parameter[] parameters = null;
-        //Method[] declaredMethods = service.getClass().getDeclaredMethods();
-        //for (Method method : declaredMethods) {
-        //    if (methodName.equals(method.getName())) {
-        //        parameters = method.getParameters();
-        //        break;
-        //    }
-        //}
-        //
-        //if (parameters == null) {
-        //    throw new MessageException("Not Found Method");
-        //}
+            MethodInvokingFactoryBean bean = new MethodInvokingFactoryBean();
 
-        MethodInvokingFactoryBean bean = new MethodInvokingFactoryBean();
+            bean.setTargetObject(service);
+            bean.setTargetMethod(methodName);
+            bean.setArguments(param);
+            bean.prepare();
+            rtn = bean.invoke();
 
-        bean.setTargetObject(service);
-        bean.setTargetMethod(methodName);
-        bean.setArguments(param);
-        bean.prepare();
-        rtn = bean.invoke();
-
-        return rtn;
+            return rtn;
+        } catch (Exception e) {
+            throw new MessageException(e.getMessage());
+        }
     }
 
 
--- src/main/java/webml/core/common/OpenController.java
+++ src/main/java/webml/core/common/OpenController.java
@@ -1,9 +1,11 @@
 package webml.core.common;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import webml.core.util.CustomMap;
 
 @Controller
@@ -11,14 +13,14 @@ import webml.core.util.CustomMap;
 public class OpenController {
 
     @GetMapping("/{d1}")
-    public String get(HttpServletRequest request, Model model, @RequestBody(required = false) CustomMap param,
+    public String get(Model model, @RequestBody(required = false) CustomMap param,
                       @PathVariable("d1") String d1) {
         model.addAttribute("param", param);
         return d1;
     }
 
     @GetMapping("/{d1}/{d2}")
-    public String get(HttpServletRequest request, Model model, @RequestBody(required = false) CustomMap param,
+    public String get(Model model, @RequestBody(required = false) CustomMap param,
                       @PathVariable("d1") String d1, @PathVariable("d2") String d2) {
         model.addAttribute("param", param);
         return d1 + "/" + d2;
