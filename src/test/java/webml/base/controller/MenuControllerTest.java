c6425a66630390a0497cbef90a36fe97ffbabaef
rainn66 <jaeseong7975@gmail.com>
2024-04-21 20:50:11 +0900

    2024.4.21 Mockito, Controller, Service Test

--- src/test/java/webml/base/controller/MenuControllerTest.java
+++ src/test/java/webml/base/controller/MenuControllerTest.java
@@ -1,13 +1,23 @@
 package webml.base.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import webml.base.core.constants.AuthorityConstants;
+import webml.base.dto.MenuDto;
 import webml.base.service.AuthorityService;
 import webml.base.service.MenuService;
 
@@ -16,18 +26,46 @@ import webml.base.service.MenuService;
 class MenuControllerTest {
 
     @InjectMocks
-    MenuController menuController;
+    private MenuController menuController;
 
     @Mock
-    MenuService menuService;
+    private MenuService menuService;
 
     @Mock
-    AuthorityService authorityService;
+    private AuthorityService authorityService;
 
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void init() {
         mockMvc = MockMvcBuilders.standaloneSetup(menuController).build();
     }
+
+    @Test
+    @DisplayName("메뉴 등록 테스트")
+    void reg_menu_test() throws Exception {
+        //given
+        MenuDto menu = MenuDto.builder()
+                .menuNm("menu")
+                .menuLink("/menu")
+                .visibleLinkYn("Y")
+                .viewAuthority(AuthorityConstants.AUTH_001)
+                .saveAuthority(AuthorityConstants.AUTH_003)
+                .parentMenuIdx(null)
+                .build();
+
+        Mockito.lenient().doNothing().when(menuService).saveMenu(Mockito.any(MenuDto.class));
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/menu")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(menu))
+        );
+
+        //then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("message", "").exists());
+    }
+
 }
--- src/test/java/webml/prj/service/RepairServiceTest.java
+++ src/test/java/webml/prj/service/RepairServiceTest.java
@@ -1,7 +1,41 @@
 package webml.prj.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class RepairServiceTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import webml.prj.dto.RepairSearchDto;
+import webml.prj.repository.PartnerRepository;
+import webml.prj.repository.RepairRepository;
+import webml.prj.repository.StoreRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class RepairServiceTest {
+
+    @InjectMocks
+    RepairService repairService;
+
+    @Mock
+    RepairRepository repairRepository;
+    @Mock
+    PartnerRepository partnerRepository;
+    @Mock
+    StoreRepository storeRepository;
+
+    @Test
+    @DisplayName("수리내역 카운트 조회 테스트")
+    void get_repair_list_cnt_test() {
+        //Mockito.doReturn(new RepairSearchDto(1, null, null, null)).when(repairRepository).countCond(Mockito.any(RepairSearchDto.class));
+        long repairListCnt = repairService.getRepairListCnt(new RepairSearchDto(1, null, null, null));
+        Assertions.assertThat(repairListCnt).isEqualTo(0);
+
+        Mockito.verify(repairRepository, Mockito.times(1)).countCond(Mockito.any(RepairSearchDto.class));
+    }
+
 
 }
